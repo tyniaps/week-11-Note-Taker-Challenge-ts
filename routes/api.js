84871f9bddd3add33ced81fe29173c5b9f9d3045
KnightNimbus <ty.s.spain@gmail.com>
2024-03-23 03:30:32 +0000

    post route for adding new notes

--- routes/api.js
+++ routes/api.js
@@ -5,3 +5,17 @@ api.get('/', (req, res) => {
 
     readFromFile('../db/db.json').then((data) => res.json(JSON.parse(data)));
 });
+
+api.post('/', (req, res) => {
+    console.info(`${req.method} request recieved to submit notes input`);
+
+    const {title, text} = req.body;
+
+    if (title && text) {
+    
+    const newNote = {
+        title,
+        text,
+    }    
+    }
+})
